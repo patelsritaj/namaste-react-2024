eb9feb58229aa6dcdc98be31be07b5009732f94b
Sritaj Kumar Patel <patelsritaj@outlook.com>
2024-03-09 00:40:27 +0530

    Added JSX syntax for declaring Header

--- Episode-03 | Foundation/App.js
+++ Episode-03 | Foundation/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//React Syntax
+/*
 const header = React.createElement("h1", {}, "Welcome To Chapter 3");
+*/
+
+//JSX - like HTML syntax in JS
+const jsxHeader = <h1>Welcome to JSX</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(header);
+root.render(jsxHeader);
