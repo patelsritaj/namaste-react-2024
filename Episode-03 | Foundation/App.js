6b71a66142e6e2766792a9d9bc6a27977f8641ad
Sritaj Kumar Patel <patelsritaj@outlook.com>
2024-03-08 23:40:19 +0530

    Added basic html and app.js files

---
+++ Episode-03 | Foundation/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const header = React.createElement("h1", {}, "Welcome To Chapter 3");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(header);
