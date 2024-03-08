b9fc56e3f3ac64c1d6af607fbfb94781b87328dd
Sritaj Kumar Patel <patelsritaj@outlook.com>
2024-03-09 02:23:08 +0530

    Added example for FunctionalComponent and Component composition

--- Episode-03 | Foundation/App.js
+++ Episode-03 | Foundation/App.js
@@ -8,10 +8,25 @@ const header = React.createElement("h1", {}, "Welcome To Chapter 3");
 
 //JSX - like HTML syntax in JS, it is transpiled before reaching to browser JS engine
 //JSX -> React.creatElement -> ReactElement-JS Object -> HTMLElement(render)
+//ReactElement
 const jsxHeader = <h1>Welcome to JSX</h1>;
 
 console.log(jsxHeader);
 
+//React Functional Component
+const HeaderUsingFunctionalComponent = () => {
+  return (
+    <div id="parent">
+      <HeaderUsingFunctionalComponentTwo />
+      <h1>Namaste React using React Functional Component</h1>;
+    </div>
+  );
+};
+
+const HeaderUsingFunctionalComponentTwo = () => {
+  return <h1>Namaste Again</h1>;
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeader);
+root.render(<HeaderUsingFunctionalComponent />);
