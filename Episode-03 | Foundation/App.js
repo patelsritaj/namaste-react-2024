1f8e65f229ffa5933ca664969305ed4e6e728faa
Sritaj Kumar Patel <patelsritaj@outlook.com>
2024-03-09 00:56:15 +0530

    Added few comments to explain the flow

--- Episode-03 | Foundation/App.js
+++ Episode-03 | Foundation/App.js
@@ -6,8 +6,11 @@ import ReactDOM from "react-dom/client";
 const header = React.createElement("h1", {}, "Welcome To Chapter 3");
 */
 
-//JSX - like HTML syntax in JS
+//JSX - like HTML syntax in JS, it is transpiled before reaching to browser JS engine
+//JSX -> React.creatElement -> ReactElement-JS Object -> HTMLElement(render)
 const jsxHeader = <h1>Welcome to JSX</h1>;
+
+console.log(jsxHeader);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
