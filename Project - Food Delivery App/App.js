104aa5dfe21fdcf3aa781a1504154693039aa42b
Sritaj Kumar Patel <patelsritaj@outlook.com>
2024-03-09 14:00:15 +0530

    Added hardcoded for showcasing examples of multiple res-cards

--- Project - Food Delivery App/App.js
+++ Project - Food Delivery App/App.js
@@ -69,6 +69,18 @@ const Body = () => {
       <div className="search">Search</div>
       <div className="res-container">
         <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
       </div>
     </div>
   );
