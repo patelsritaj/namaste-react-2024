2956eb27b79c6996f3890b3aad27af84320ed2a8
Sritaj Kumar Patel <patelsritaj@outlook.com>
2024-03-12 22:46:10 +0530

    Added Swiggy payload for a restaurant

--- Project - Food Delivery App/App.js
+++ Project - Food Delivery App/App.js
@@ -63,6 +63,75 @@ const RestaurantCard = () => {
   );
 };
 
+const resObj = {
+  info: {
+    id: "712380",
+    name: "Chicago Pizza",
+    cloudinaryImageId: "80f040545552605e33eba09f8fa30be9",
+    locality: "khb colony",
+    areaName: "Koramangala",
+    costForTwo: "₹500 for two",
+    cuisines: ["Pizzas", "Fast Food", "Italian"],
+    avgRating: 4.1,
+    parentId: "60277",
+    avgRatingString: "4.1",
+    totalRatingsString: "100+",
+    sla: {
+      deliveryTime: 40,
+      lastMileTravel: 6.7,
+      serviceability: "SERVICEABLE",
+      slaString: "40-45 mins",
+      lastMileTravelString: "6.7 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2024-03-13 01:00:00",
+      opened: true,
+    },
+    badges: {},
+    isOpen: true,
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {},
+        textBased: {},
+        textExtendedBadges: {},
+      },
+    },
+    aggregatedDiscountInfoV3: {
+      header: "₹125 OFF",
+      subHeader: "ABOVE ₹499",
+      discountTag: "FLAT DEAL",
+    },
+    orderabilityCommunication: {
+      title: {},
+      subTitle: {},
+      message: {},
+      customIcon: {},
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {},
+      },
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: {},
+  },
+  analytics: {
+    context: "seo-data-61e73e71-471e-4175-84ca-cfab6814a6ed",
+  },
+  cta: {
+    link: "https://www.swiggy.com/restaurants/chicago-pizza-khb-colony-koramangala-bangalore-712380",
+    text: "RESTAURANT_MENU",
+    type: "WEBLINK",
+  },
+  widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+};
+
 const Body = () => {
   return (
     <div className="body">
