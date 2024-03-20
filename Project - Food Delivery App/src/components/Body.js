e2f964a4a588de238492999ea70ff24185d474c8
Sritaj Kumar Patel <patelsritaj@outlook.com>
2024-03-20 22:56:13 +0530

    Removed hardcoded data and used useEffect and fetch to call live api to render the rest list

--- Project - Food Delivery App/src/components/Body.js
+++ Project - Food Delivery App/src/components/Body.js
@@ -1,9 +1,27 @@
 import RestaurantCard from "./RestaurantCard";
 import resObj from "../utils/mockdata";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
-  const [restaurantObj, setRestaurantObj] = useState(resObj);
+  const [restaurantObj, setRestaurantObj] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    console.log(json);
+
+    setRestaurantObj(
+      json.data?.cards?.[4].card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
 
   return (
     <div className="body">
