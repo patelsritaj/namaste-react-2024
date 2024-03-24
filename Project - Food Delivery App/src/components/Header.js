7727ccec07ce47ba0e1add8bac30491e57aee2eb
Sritaj Kumar Patel <patelsritaj@outlook.com>
2024-03-24 12:17:32 +0530

    Added a login button to header and using useState changed the text

--- Project - Food Delivery App/src/components/Header.js
+++ Project - Food Delivery App/src/components/Header.js
@@ -1,6 +1,9 @@
+import { useEffect, useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -16,6 +19,14 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="loginbtn"
+            onClick={() => {
+              setBtnName("Logout");
+            }}
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
