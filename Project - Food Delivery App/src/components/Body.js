d018ddcc4b3c6e38ec874cd798d9a40fd395b539
Sritaj Kumar Patel <patelsritaj@outlook.com>
2024-03-26 20:46:57 +0530

    Added swiggy api to constants and updated the same

--- Project - Food Delivery App/src/components/Body.js
+++ Project - Food Delivery App/src/components/Body.js
@@ -1,6 +1,7 @@
 import RestaurantCard from "./RestaurantCard";
 import { useEffect, useState } from "react";
 import ShimmerUI from "./Shimmer";
+import { SWIGGY_LIVE_API_URL } from "../utils/constants";
 
 const Body = () => {
   const [restaurantObj, setRestaurantObj] = useState([]);
@@ -12,9 +13,7 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    );
+    const data = await fetch(SWIGGY_LIVE_API_URL);
 
     const json = await data.json();
 
--- Project - Food Delivery App/src/utils/constants.js
+++ Project - Food Delivery App/src/utils/constants.js
@@ -3,3 +3,6 @@ export const CDN_URL =
 
 export const LOGO_URL =
   "https://www.clipartmax.com/png/small/111-1118804_android-food-delivery-apps.png";
+
+export const SWIGGY_LIVE_API_URL =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
